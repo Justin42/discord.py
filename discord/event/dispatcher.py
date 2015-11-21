f01018f7bcf86c2803f5afc05d414d6a87501b8d
Justin Baldwin <justin.d.baldwin91@gmail.com>
2015-11-21 17:23:43 -0500

    woops

--- discord/event/dispatcher.py
+++ discord/event/dispatcher.py
@@ -1,6 +1,7 @@
 import logging
 import threading
 
+from discord.event.event_type import EventType
 from ..event.listener import EventListener
 from .events import Event
 
@@ -12,15 +13,17 @@ class EventDispatcher:
         self.dispatch_lock = threading.RLock()
         self.listeners = []
 
-    def dispatch(self, event: Event): # TODO Call on_event to add client attribute
+    def dispatch(self, event: Event):
         with self.dispatch_lock:
             log.debug("Dispatching event {}".format(event.data))
             event_method = '_'.join(('on', event.type.name.lower()))
             # noinspection PyBroadException
-            try:
-                getattr(self, event_method, self.unhandled_event)(event)
-            except Exception as e:
-                getattr(self, 'on_error')(event)
+            for listener in self.listeners:
+                try:
+                    getattr(listener, event_method, self.unhandled_event)(event)
+                except Exception as e:
+                    event = Event(EventType.ERROR, event.data, event.client)
+                    self.dispatch(event)
         pass
 
     def add_listener(self, listener: EventListener):
